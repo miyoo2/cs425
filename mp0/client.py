add72de22ccede40be93797ba86a61a4fffc387d
js345 <1994yxf@gmail.com>
2015-01-26 14:31:36 -0600

    Socket File
    Server sent files to all clients

--- mp0/client.py
+++ mp0/client.py
@@ -5,5 +5,13 @@ host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
-print s.recv(1024)
+print "Successfully connected to ", (host, port)
+filename = 'client_' + s.recv(1024)
+with open(filename,'w') as f:
+	line = s.recv(1024)
+	while line:
+		f.write(line)
+		line = s.recv(1024)
+f.close()
+print "File written to current dir\n"
 s.close                     # Close the socket when done
